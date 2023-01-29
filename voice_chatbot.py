f275cb9f072a50b027857ae4d8c91dd0ed8c1fd8
TRAN QUOC HUY <huy76229@gmail.com>
2023-01-29 12:31:19 +0900

    first commit

---
+++ voice_chatbot.py
@@ -0,0 +1,43 @@
+import openai
+import pyttsx3
+import speech_recognition as sr
+
+
+openai.api_key = "OPENAI_API_KEY"
+
+engine = pyttsx3.init()
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+conversation = ""
+user_name = "Carl"
+bot_name = "Ola"
+
+while True:
+    with mic as source:
+        print("\nlistening...")
+        r.adjust_for_ambient_noise(source, duration=0.2)
+        audio = r.listen(source)
+    print("no longer listening.\n")
+
+    try:
+        user_input = r.recognize_google(audio)
+        print("user input : " + user_input)
+    except Exception as e:
+        print(e)
+        continue
+
+    prompt = user_name + ": " + user_input + "\n" + bot_name + ": "
+
+    conversation += prompt
+
+    response = openai.Completion.create(engine='text-davinci-001', prompt=conversation, max_tokens=200)
+    response_str = response["choices"][0]["text"].replace("\n", "")
+    response_str = response_str.split(user_name + ": ", 1)[0].split(bot_name + ": ", 1)[0]
+
+    conversation += response_str + "\n"
+    print(response_str)
+
+    engine.say(response_str)
+    engine.runAndWait()
